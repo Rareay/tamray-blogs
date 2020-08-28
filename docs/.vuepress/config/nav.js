37d05118fd4a03cc8fa047d028241a80f8d684b2
tamray <www.tamray.cn>
2020-08-28 17:26:49 +0800

    init.

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,61 @@
+module.exports = [
+  {text: '首页', link: '/'},
+  {
+    text: '机器视觉',
+    items: [
+      {text: 'Opencv', link: '/pages/220ed1/'},
+      {text: 'Slame',  link: '/pages/9fc2c3/'},
+    ]
+  },
+  {
+    text: '编程语言',
+    items: [
+      {text: 'Rust',   link: '/pages/284548/'},
+      {text: 'C++',    link: '/pages/49b2e8/'},
+      {text: 'Python', link: '/pages/b9fb7b/'},
+    ]
+  },
+  {
+    text: 'Linux',
+    link: '/pages/4f6102/',
+  },
+  {
+    text: '应用',
+    items: [
+      {text: 'Qt',    link: '/pages/5ab9cc/'},
+      {text: 'Boost', link: '/pages/7a8b47/'},
+    ]
+  },
+  {
+    text: '技术',
+    link: '/pages/7bc2e3/',
+  },
+  //{
+  //  text: '其它',
+  //  items: [
+  //    {test: '随写',      link: ''},
+  //    {test: '心理学文章', link: ''},
+  //    {test: '经济学文章', link: ''},
+  //  ]
+  //}
+
+  {text: '关于', link: '/about/'},
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+    items: [
+      {text: '网站', link: '/pages/beb6c0bd8a66cea6/'},
+      {text: '资源', link: '/pages/eee83a9211a70f9d/'},
+      {text: 'Vue资源', link: '/pages/12df8ace52d493f6/'},
+    ]
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      {text: '分类', link: '/categories/'},
+      {text: '标签', link: '/tags/'},
+      {text: '归档', link: '/archives/'},
+    ]
+  }
+]
