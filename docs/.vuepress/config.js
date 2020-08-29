e3fcd3341d72657519e804ca7331756586a77652
Tamray <985480276@qq.com>
2020-08-29 16:40:26 +0800

    增加了代码类型

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -96,7 +96,7 @@ module.exports = {
     },
     footer:{ // 页脚信息
       createYear: 2020, // 博客创建年份
-      copyrightInfo:  '个人记录', // 博客版权信息，支持a标签
+      copyrightInfo:  '个人记录 All Rights Reserved. 备案号:渝ICP备20002389', // 博客版权信息，支持a标签
     }
   },
   
