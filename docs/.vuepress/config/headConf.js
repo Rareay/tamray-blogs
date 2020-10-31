7f6a7bc0a59268d86053c4e5318926121e9c7718
Tamray <985480276@qq.com>
2020-11-01 00:25:34 +0800

    add some

--- docs/.vuepress/config/headConf.js
+++ docs/.vuepress/config/headConf.js
@@ -2,7 +2,7 @@
 
 module.exports = [
   ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
-  ['meta', { name: 'keywords', content: 'Opencv,Slam,Rust,c++,Python,Linux,Ubuntu'}],
+  ['meta', { name: 'keywords', content: 'Opencv,Slam,Rust,c++,Python,Linux,Ubuntu,Caffe,Qt,Boost'}],
   ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc'}],// 百度统计的站点拥有者验证
   ['meta', { name: 'theme-color', content: '#11a8cd'}], // 移动浏览器主题颜色
   ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
@@ -10,5 +10,6 @@ module.exports = [
   ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
   ['link', { rel: 'icon', href: '/favicon.ico' }],
   ['link', { rel: 'manifest', href: '/manifest.json' }],
-  ['meta', { name: 'theme-color', content: '#ffffff' }]
+  ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ['script', { type: 'text/javascript', src: '/assets/push.js' }]
 ];
