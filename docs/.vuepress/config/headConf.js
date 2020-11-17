427ef91b8b7256a735c8617fd9a07e05b2ba7b1b
tamray <www.tamray.cn>
2020-11-17 14:13:11 +0800

    删除了背景图

--- docs/.vuepress/config/headConf.js
+++ docs/.vuepress/config/headConf.js
@@ -10,6 +10,6 @@ module.exports = [
   ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
   ['link', { rel: 'icon', href: '/favicon.ico' }],
   ['link', { rel: 'manifest', href: '/manifest.json' }],
-  ['meta', { name: 'theme-color', content: '#ffffff' }],
-  ['script', { type: 'text/javascript', src: '/assets/push.js' }]
+  ['meta', { name: 'theme-color', content: '#ffffff' }]
+  //['script', { type: 'text/javascript', src: '/assets/push.js' }]
 ];
--- docs/index.md
+++ docs/index.md
@@ -3,7 +3,7 @@ home: true
 # heroImage: /img/web.png
 heroText: Tamray's blog
 tagline: Keep hungry. Keep foolish.
-bannerBg: "/img/bg3.jpg"
+#bannerBg: "/img/bg3.jpg"
 # actionText: 立刻进入 →
 # actionLink: /web/
 # bannerBg: auto # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
